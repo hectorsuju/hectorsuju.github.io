2fe6088a5400985f5a56a514bbae8f6ef8cb188e
hectorsuju <53528954+hectorsuju@users.noreply.github.com>
2026-05-08 15:46:32 +0200

    Inicial commit

---
+++ sendmail.php
@@ -0,0 +1,25 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: ' . $email; 
+    $to = 'hola@conmuchococo.com'; 
+    $subject = 'Formulario-Web';
+    $human = $_POST['human'];
+
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+	    if ($name != '' && $email != '') {
+	        if ($human == '4') {                 
+	            if (mail ($to, $subject, $body, $from)) { 
+	            echo '<p>¡Tu mensaje se ha enviado correctamente!</p>';
+	        } else { 
+	            echo '<p>¡Ups! Algo ha salido mal, porfavor vuelve a intentarlo.</p>'; 
+	        } 
+	    } else if ($human != '4') {
+	        echo '<p>Has respondido de forma incorrecta la pregunta anti-spam</p>';
+	    }
+	    } else {
+	        echo '<p>¡Todos los campos son requeridos!</p>';
+	    }
+?>
